bb4ddb253454de4a7c5464eb8e1be5be2b8272c1
Alexandre Amourdom <alexandre.amourdom@gmail.com>
2022-11-17 11:02:19 +0100

    changement de couleurs en fonction de la luminosité

--- app.js
+++ app.js
@@ -25,6 +25,50 @@ function populateUI() {
   document.body.style.background = `linear-gradient(${gradientData.angle}deg,${gradientData.colors[0]},${gradientData.colors[1]})`;
 
   rangeLabelValue.textContent = `${gradientData.angle}°`;
+
+  adaptInputsColor();
 }
 
 populateUI();
+
+function adaptInputsColor() {
+  colorLabels.forEach((label) => {
+    const hexColor = label.textContent.replace("#", "");
+
+    const red = parseInt(hexColor.slice(0, 2), 16);
+
+    const green = parseInt(hexColor.slice(2, 4), 16);
+    const blue = parseInt(hexColor.slice(4, 6), 16);
+
+    const yiq = (red * 299 + green * 587 + blue * 144) / 1000;
+
+    console.log(yiq);
+
+    if (yiq >= 128) {
+      label.style.color = "#111";
+    } else {
+      label.style.color = "#f1f1f1";
+    }
+  });
+}
+
+const rangeInput = document.querySelector(".inp-range");
+
+rangeInput.addEventListener("input", handleInclination);
+
+function handleInclination() {
+  gradientData.angle = rangeInput.value;
+  rangeLabelValue.textContent = `${gradientData.angle}°`;
+  populateUI();
+}
+
+colorPickerInputs.forEach((input) =>
+  input.addEventListener("input", colorInputModification)
+);
+
+function colorInputModification(e) {
+  const currentIndex = colorPickerInputs.indexOf(e.target);
+  console.log(currentIndex);
+  gradientData.colors[currentIndex] = e.target.value.toUpperCase();
+  populateUI();
+}
